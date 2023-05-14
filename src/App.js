6705d2388c0bd1f07f6d23d1cb396f403fdf13f9
SarahParkSehyun <101391544+SarahParkSehyun@users.noreply.github.com>
2023-05-14 15:46:59 +0900

    React Router 기본

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import RouteTest from './components/RouteTest';
+import Home from './pages/Home.js'
+import New from './pages/New.js'
+import Edit from './pages/Edit.js'
+import Diary from './pages/Diary.js'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
