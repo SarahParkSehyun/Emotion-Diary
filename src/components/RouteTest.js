7ad35d1a25e1b6b2d5ae3eed28dbed68434f9e54
박세현 <sarah1918@naver.com>
2023-05-14 15:54:57 +0900

    .

--- src/components/RouteTest.js
+++ src/components/RouteTest.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from 'react-router-dom'
 
 const RouteTest = () => {
@@ -16,23 +15,4 @@ const RouteTest = () => {
   );
 };
 
-=======
-import { Link } from 'react-router-dom'
-
-const RouteTest = () => {
-  return (
-    <>
-      <Link to={"/"}>HOME</Link>
-      <br />
-      <Link to={"/diary"}>Diary</Link>
-      <br />
-      <Link to={"/new"}>NEW</Link>
-      <br />
-      <Link to={"/edit"}>Edit</Link>
-      <br />
-    </>
-  );
-};
-
->>>>>>> 6705d2388c0bd1f07f6d23d1cb396f403fdf13f9
 export default RouteTest;
