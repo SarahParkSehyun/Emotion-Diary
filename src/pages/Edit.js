046f544aaee500dccd55bcce3ae8adc49b9eb3dc
박세현 <sarah1918@naver.com>
2023-05-14 15:54:47 +0900

    .

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Edit = () => {
   return (<div>
     <h1>Edit</h1>
@@ -6,15 +5,3 @@ const Edit = () => {
   </div>
   );
 };
-
-=======
-const Edit = () => {
-  return (<div>
-    <h1>Edit</h1>
-    <p>이곳은 일기 수정 페이지 입니다.</p>
-  </div>
-  );
-};
-
->>>>>>> 6705d2388c0bd1f07f6d23d1cb396f403fdf13f9
-export default Edit;
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const New = () => {
   return (<div>
     <h1>New</h1>
@@ -6,15 +6,3 @@ const New = () => {
   </div>
   );
 };
-
-=======
-const New = () => {
-  return (<div>
-    <h1>New</h1>
-    <p>이곳은 일기 작성 페이지 입니다.</p>
-  </div>
-  );
-};
-
->>>>>>> 6705d2388c0bd1f07f6d23d1cb396f403fdf13f9
-export default New;
