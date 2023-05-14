06b1f757c2072d95ca0c0150bac473a61016c364
박세현 <sarah1918@naver.com>
2023-05-14 16:55:32 +0900

    프로젝트 기초 공사

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import RouteTest from './components/RouteTest';
+
 import Home from './pages/Home.js'
 import New from './pages/New.js'
 import Edit from './pages/Edit.js'
 import Diary from './pages/Diary.js'
 
 function App() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
   return (
     <BrowserRouter>
       <div className='App'>
         <h2>App.js</h2>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
+
       </div>
 
     </BrowserRouter>
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router";
 const Diary = () => {
+  const { id } = useParams();
   return (<div>
-    const {id}=useParams();
 
     <h1>Diary</h1>
     <p>이곳은 일기 상세 페이지 입니다.</p>
