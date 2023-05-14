d03865fc7a43d140e72fba794f8cac8901734c7b
박세현 <sarah1918@naver.com>
2023-05-14 19:58:16 +0900

    프로젝트 기초공사 1

--- src/App.js
+++ src/App.js
@@ -7,14 +7,33 @@ import New from './pages/New.js'
 import Edit from './pages/Edit.js'
 import Diary from './pages/Diary.js'
 
+//components
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
+
 function App() {
-  const env = process.env;
-  env.PUBLIC_URL = env.PUBLIC_URL || "";
+  //const env = process.env;
+  //env.PUBLIC_URL = env.PUBLIC_URL || "";
   return (
     <BrowserRouter>
-      <div className='App'>
+      <div className="App">
+        <MyHeader headText={"App"}
+          leftChild={<MyButton text={'왼쪽 버튼'} onClick={() => alert("왼쪽 클릭")} />}
+          rightChild={<MyButton text={'오른쪽 버튼'} onClick={() => alert('오른쪽 클릭')} />}
+        />
         <h2>App.js</h2>
 
+        <MyButton text={'버튼'}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+        <MyButton text={'버튼'}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+        <MyButton text={'버튼'}
+          onClick={() => alert("버튼 클릭")}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
